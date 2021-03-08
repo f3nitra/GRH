3ed962aa0c4449d026a3b7981c1c509aa7c39c02
Fenitra Andriamaromanana <f3nitraandriamaromanana@gmail.com>
2021-03-08 20:13:58 +0300

    Mise à jour ce 08/03/2021

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,19 @@
 @section('content')
 <h1>WELCOME</h1>
 <hr>
+<div class="card-deck">
 @foreach($articles as $article)
     <div class="card border-primary mb-3" style="max-width: 20rem;">
         <div class="card-header"><h2>{{ $article->title }}</h2></div>
         <div class="card-body">
         <h4 class="card-title">By {{ $article->author }}</h4>
-        <p class="card-text">{{ $article->content }}</p>
+        <p class="card-text" id="lirelasuite">{{ $article->content }}</p>
+        <p class="card-footer"><small class="text-muted">Created at {{ $article->created_at }}</small></p>
         </div>
     </div>
-@endforeach
+    @endforeach
+</div>    
+
+
 
 @endsection
