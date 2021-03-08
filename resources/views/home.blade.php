7f0d1529c8c2f8a0a6849aa246becd5263bd328d
Fenitra Andriamaromanana <f3nitraandriamaromanana@gmail.com>
2021-03-08 21:26:42 +0300

    Mise à jour de la page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 <h1>WELCOME</h1>
 <hr>
-<div class="card-deck">
+<div class="card-columns">
 @foreach($articles as $article)
     <div class="card border-primary mb-3" style="max-width: 20rem;">
+        <img src="http://placehold.it/350x150" class="card-img-top" alt="...">
         <div class="card-header"><h2>{{ $article->title }}</h2></div>
         <div class="card-body">
         <h4 class="card-title">By {{ $article->author }}</h4>
-        <p class="card-text" id="lirelasuite">{{ $article->content }}</p>
+        <p class="card-text"> {{ $article->content }}</p><p>...</p>
         <p class="card-footer"><small class="text-muted">Created at {{ $article->created_at }}</small></p>
         </div>
     </div>
     @endforeach
 </div>    
-
-
-
 @endsection
